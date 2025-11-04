d7701c8d4f14844083e5661e3b991a46a2ebff58
SRITHAR <srithar6757@gmail.com>
2025-11-04 19:20:27 +0530

    Implement random password generator
    The program generates a random password of specified length, ensuring it includes at least one lowercase letter, one uppercase letter, and one number.

--- EXP2/Question_9.java
+++ EXP2/Question_9.java
@@ -1,9 +1,45 @@
-
+import java.util.*;
 public class Question_9 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+Random random = new Random();
+char small[]=
+{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
+'v','w','x','y','z'};
+char capital[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+System.out.print("Enter the password length: ");
+int length = sc.nextInt();
+if (length<3)
+{
+System.out.println("Minimum length is 3 ");
+return;
 }
+String result = "";
+result += small[random.nextInt(small.length)];
+result += capital[random.nextInt(capital.length)];
+result += numbers[random.nextInt(numbers.length)];
+	for (int i = 3; i < length; i++)
+{
+int choice = random.nextInt(3); // 0 for small, 1 for capital, 2 for
+numbers
+if (choice == 0)
+{
+result += small[random.nextInt(small.length)];
+}
+else if (choice == 1)
+{
+result += capital[random.nextInt(capital.length)];
+}
+else
+{ // choice == 2
+result += numbers[random.nextInt(numbers.length)];
+}
+}
+System.out.println(result);
+}
+}
+
+
